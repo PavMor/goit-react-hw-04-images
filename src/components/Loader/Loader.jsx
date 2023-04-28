135d0948be770f6abb7ac35c800a5b5d5981fdb0
Your Name <pavlo.morozenko@ukr.net>
2023-04-28 20:41:36 +0200

    libraries installing and src files moving in

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import { MdOutlineCameraswitch } from 'react-icons/md';
+import { Wrapper } from './Loader.styled';
+
+export const Loader = () => {
+  return (
+    <Wrapper>
+      <MdOutlineCameraswitch size={56} />
+    </Wrapper>
+  );
+}
