a117fb26249f600cf0b7aba75f278b013d1da5fd
Your Name <pavlo.morozenko@ukr.net>
2023-04-29 19:45:57 +0200

    1

--- src/components/Loader/Loader.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100px;
-    color: var(--accent-bg-color);
-`;
