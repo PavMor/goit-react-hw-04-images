3d29116c581e74311394f611f166eb7c241cbdad
Your Name <pavlo.morozenko@ukr.net>
2023-04-29 11:26:46 +0200

    ImageGallery(Item/List)

---
+++ src/components/ImageGallery/ImageGalleryList/ImageGalleryList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ImageGalleryItem } from '../ImageGalleryItem';
+import s from './ImageGalleryList.module.css';
+
+export function ImageGalleryList({ images }) {
+  return (
+    <ul className={s.ImageGallery}>
+      {images.map(({ webformatURL, largeImageURL, tags }, index) => (
+        <ImageGalleryItem
+          key={index}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          tags={tags}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ImageGalleryList.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
